db827ebc258ca08c8f5a121260034ed84d1583ca
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-02-13 16:19:10 +0100

    qcom/gps: add TARGET_PROVIDES_GPS_LOC_API
    Change-Id: Ia3126d6fceedad4b6fc19e4695c8996aaea65c53

--- utils/Android.mk
+++ utils/Android.mk
@@ -1,3 +1,4 @@
+ifneq ($(TARGET_PROVIDES_GPS_LOC_API),true)
 ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 ifneq ($(BUILD_TINY_ANDROID),true)
 #Compile this library only for builds with the latest modem image
@@ -42,4 +43,4 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 include $(BUILD_SHARED_LIBRARY)
 endif # not BUILD_TINY_ANDROID
 endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
-
+endif#TARGET_PROVIDES_GPS_LOC_API
