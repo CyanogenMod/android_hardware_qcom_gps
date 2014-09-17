53cc90b2ccaa3676deadbe4b321e713efe45dc8b
Tom Marshall <tdm@cyngn.com>
2014-09-17 16:07:55 -0700

    gps: Use project pathmap
     * Include if either BOARD_USES_QCOM_HARDWARE is not set or if the
       GPS variant matches.
    
    Change-Id: I21d0cd47f09ee2ab4d63c1717b4513c521e890d7

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-LOCAL_PATH := $(call my-dir)
+ifeq ($(TARGET_GPS_HAL_PATH),)
+
+ifeq ($(call my-dir),$(call project-path-for,qcom-gps))
 
 ifneq ($(filter msm8960 apq8064 ,$(TARGET_BOARD_PLATFORM)),)
     #For msm8960/apq8064 targets
@@ -9,4 +10,7 @@ else
     GPS_DIRS=core utils loc_api platform_lib_abstractions etc
     include $(call all-named-subdir-makefiles,$(GPS_DIRS))
 endif #TARGET_BOARD_PLATFORM
-endif #BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
+
+endif
+
+endif
