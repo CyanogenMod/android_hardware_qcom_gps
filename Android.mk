44cd27bad5cf2c87786682d8f21a49d457787596
Tom Marshall <tdm@cyngn.com>
2014-10-01 16:35:55 -0700

    gps: Use project pathmap
     * Include if either BOARD_USES_QCOM_HARDWARE is not set or if the
       GPS variant matches.
    
    Change-Id: I21d0cd47f09ee2ab4d63c1717b4513c521e890d7

--- Android.mk
+++ Android.mk
@@ -1,5 +1,12 @@
-ifeq ($(call my-dir),$(QCOM_GPS_PATH))
+# Must always include this if BOARD_USES_QCOM_HARDWARE is unset
+ifeq ($(BOARD_USES_QCOM_HARDWARE),)
+USE_QCOM_GPS := true
+endif
 
+ifeq ($(call my-dir),$(call project-path-for,qcom-gps))
+USE_QCOM_GPS := true
+endif
+
+ifeq ($(USE_QCOM_GPS),true)
 include $(call all-subdir-makefiles)
-
 endif
