616f89551e81e6869fadd1038ad626041b92a278
Ethan Chen <intervigil@gmail.com>
2014-10-03 14:59:06 -0700

    gps: Expect build system to always have a default QCOM variant
    Change-Id: Iafe2243b335e67b3a719484dc2faf0fa5ef01651

--- Android.mk
+++ Android.mk
@@ -1,12 +1,3 @@
-# Must always include this if BOARD_USES_QCOM_HARDWARE is unset
-ifeq ($(BOARD_USES_QCOM_HARDWARE),)
-USE_QCOM_GPS := true
-endif
-
 ifeq ($(call my-dir),$(call project-path-for,qcom-gps))
-USE_QCOM_GPS := true
-endif
-
-ifeq ($(USE_QCOM_GPS),true)
 include $(call all-subdir-makefiles)
 endif
