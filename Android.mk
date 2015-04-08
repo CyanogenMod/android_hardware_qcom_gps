51025d58ad3a60bd2c5caed17869a9bb360ccaaf
Tom Cherry <tomcherry@google.com>
2015-04-07 17:06:52 -0700

    Do not build GPS HAL for msm8916
    msm8916's GPS HAL resides in a different location in the source tree
    
    Change-Id: I58ebad34ea19f0e498b9532f9be88f052416a96a

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter msm8960 apq8064 ,$(TARGET_BOARD_PLATFORM)),)
     #For msm8960/apq8064 targets
     include $(call all-named-subdir-makefiles,msm8960)
-else
+else ifeq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
     #For all other targets
     GPS_DIRS=core utils loc_api platform_lib_abstractions etc
     include $(call all-named-subdir-makefiles,$(GPS_DIRS))
