02685cbf3e54751000270b430934ebece3eeaffa
Steve Paik <spaik@google.com>
2015-07-15 16:51:13 -0700

    Workaround for adp8064
    This is a squash of I8cf758c302b3189064c83ce43ec9bcaf25daeb80
    and If4e9c742eba236a18c1e6647c6002dc9d46407d4
    
    Change-Id: I191aa0f5fafd3862806468e77237da807d49924f

--- Android.mk
+++ Android.mk
@@ -1,26 +1,29 @@
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-LOCAL_PATH := $(call my-dir)
+# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ifneq ($(TARGET_BOARD_AUTO),true)
+  ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+  LOCAL_PATH := $(call my-dir)
 
-ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
+    ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
 
-ifneq ($(filter msm8960 apq8064 ,$(TARGET_BOARD_PLATFORM)),)
-    #For msm8960/apq8064 targets
-    include $(call all-named-subdir-makefiles,msm8960)
-else ifneq ($(filter msm8974 ,$(TARGET_BOARD_PLATFORM)),)
-    #For msm8974 target
-    include $(call all-named-subdir-makefiles,msm8974)
-else ifneq ($(filter msm8084 ,$(TARGET_BOARD_PLATFORM)),)
-    #For msm8084 target
-    include $(call all-named-subdir-makefiles,msm8084)
-else ifeq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
-    #For all other targets
-    GPS_DIRS=core utils loc_api platform_lib_abstractions etc
-    include $(call all-named-subdir-makefiles,$(GPS_DIRS))
-endif #TARGET_BOARD_PLATFORM
+      ifneq ($(filter msm8960 apq8064 ,$(TARGET_BOARD_PLATFORM)),)
+        #For msm8960/apq8064 targets
+        include $(call all-named-subdir-makefiles,msm8960)
+      else ifneq ($(filter msm8974 ,$(TARGET_BOARD_PLATFORM)),)
+        #For msm8974 target
+        include $(call all-named-subdir-makefiles,msm8974)
+      else ifneq ($(filter msm8084 ,$(TARGET_BOARD_PLATFORM)),)
+        #For msm8084 target
+        include $(call all-named-subdir-makefiles,msm8084)
+      else ifeq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
+        #For all other targets
+        GPS_DIRS=core utils loc_api platform_lib_abstractions etc
+        include $(call all-named-subdir-makefiles,$(GPS_DIRS))
+      endif #TARGET_BOARD_PLATFORM
 
-else ifeq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
-    GPS_DIRS=core utils loc_api platform_lib_abstractions etc
-    include $(call all-named-subdir-makefiles,$(GPS_DIRS))
-endif #BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET
+    else ifeq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
+      GPS_DIRS=core utils loc_api platform_lib_abstractions etc
+      include $(call all-named-subdir-makefiles,$(GPS_DIRS))
+    endif #BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET
 
-endif #BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
+  endif #BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
+endif
