0e7c6a8c8fb22ca805389154ca2b0b71b0f6ca5c
Ethan Chen <intervigil@gmail.com>
2016-01-18 11:14:39 -0800

    gps: Simplify guard makefile
    * USE_DEVICE_SPECIFIC_GPS != true is implied by
      $(project-path-for,qcom-gps) == $(my-dir)
    * TARGET_GPS_HAL_PATH is deprecated
    * Makefile already checks for 8960, 8064, 8974, 8084, 8916, so the
      QCOM_BOARD_PLATFORMS check is redundant as well.
    
    Change-Id: I9f831b375a33ab3aa7efc96e3b3a2a15ef4324e0

--- Android.mk
+++ Android.mk
@@ -1,13 +1,5 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-gps))
 
-ifneq ($(USE_DEVICE_SPECIFIC_GPS),true)
-
-ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
-
-ifeq ($(TARGET_GPS_HAL_PATH),)
-
-ifeq ($(call my-dir),$(call project-path-for,qcom-gps))
-
 ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
 
 ifneq ($(filter msm8960 apq8064 ,$(TARGET_BOARD_PLATFORM)),)
@@ -29,13 +21,5 @@ else ifeq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
     GPS_DIRS=core utils loc_api platform_lib_abstractions etc
     include $(call all-named-subdir-makefiles,$(GPS_DIRS))
 endif #BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET
-
-endif
-
-endif
-
-endif
-
-endif
 
 endif
