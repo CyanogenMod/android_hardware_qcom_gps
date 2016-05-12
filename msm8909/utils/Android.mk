7fd74c908ee91725086c0ffb98a024a6695d871d
Kevin Tang <zhikait@codeaurora.org>
2016-05-12 13:31:25 -0700

    makefile change to enable CLANG
    Change-Id: Ia0dac74b8fda3bd57e9b7be840b4a6009da3e287

--- msm8909/utils/Android.mk
+++ msm8909/utils/Android.mk
@@ -27,8 +27,7 @@ LOCAL_SRC_FILES += \
 
 LOCAL_CFLAGS += \
      -fno-short-enums \
-     -D_ANDROID_ \
-     -std=c++11
+     -D_ANDROID_
 
 ifeq ($(TARGET_BUILD_VARIANT),user)
    LOCAL_CFLAGS += -DTARGET_BUILD_VARIANT_USER
@@ -60,7 +59,6 @@ LOCAL_COPY_HEADERS:= \
    loc_misc_utils.h
 
 LOCAL_MODULE := libgps.utils
-LOCAL_CLANG := false
 
 LOCAL_MODULE_TAGS := optional
 
