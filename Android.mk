6870cadbd57d3f005b28cb063b7af5bc7ad92ce9
Chirayu Desai <chirayudesai1@gmail.com>
2016-08-24 19:23:25 +0530

    Revert "msm8974: remove from top level makefile"
    This reverts commit 53bf15aab71461f81e27e6f5176afcd1a29af7d4.
    
    Change-Id: Idbb7520f3d47a8bca480260c7a06c3ca8b911f44

--- Android.mk
+++ Android.mk
@@ -8,6 +8,9 @@ ifneq ($(TARGET_BOARD_AUTO),true)
       ifneq ($(filter msm8960 apq8064 ,$(TARGET_BOARD_PLATFORM)),)
         #For msm8960/apq8064 targets
         include $(call all-named-subdir-makefiles,msm8960)
+      else ifneq ($(filter msm8974 ,$(TARGET_BOARD_PLATFORM)),)
+        #For msm8974 target
+        include $(call all-named-subdir-makefiles,msm8974)
       else ifneq ($(filter msm8084 ,$(TARGET_BOARD_PLATFORM)),)
         #For msm8084 target
         include $(call all-named-subdir-makefiles,msm8084)
